984a4f65c18eb134ba71ff21209db26eb3b4f916
Juan <you@example.com>
2025-07-04 12:18:34 -0300

    se instalo react-router y se crearon las rutas necesarias - falta definir los componentes de cada una

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Routes, Route, BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App/>}/>
+        <Route path='/category/:id'/>
+        <Route path='/song/:id'/>
+        <Route path='/favoritos'/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
